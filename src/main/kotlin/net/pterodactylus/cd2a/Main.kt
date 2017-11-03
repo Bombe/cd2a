05ec06b91e42216bd7213ebe9bd83301a82b0c9c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2017-11-03 11:37:56 +0100

    Keep .mov files, too

--- src/main/kotlin/net/pterodactylus/cd2a/Main.kt
+++ src/main/kotlin/net/pterodactylus/cd2a/Main.kt
@@ -183,7 +183,7 @@ fun Content.remove() {
 }
 
 fun String.isVideo() = toLowerCase()
-		.split(".").last() in listOf("mp4", "mpg", "mkv", "avi")
+		.split(".").last() in listOf("mp4", "mpg", "mkv", "avi", "mov")
 
 fun String.isUrl() = toLowerCase().endsWith(".url")
 
